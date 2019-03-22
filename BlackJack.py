e54cfe98366d05a5425437e3ea9deb7098417164
DragonPaint <47928025+DragonPaint@users.noreply.github.com>
2019-03-22 16:59:27 +0100

    Add files via upload
    It refuses to save

--- BlackJack.py
+++ BlackJack.py
@@ -5,8 +5,7 @@ deck = ["null"
         "HA", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "HJ", "HQ", "HK",
         "DA", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "DJ", "DQ", "DK",
         "CA", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "CJ", "CQ", "CK"]
-numbers = 0
-suits = ["S", "H", "D", "C"]
+numbers = None
 used = []
 player = 0
 player_name = []
@@ -27,18 +26,18 @@ def blackjack_names():
 def blackjack_main(player_count):
     player_cards.clear()
     used_cards = [""]
-    computer_cards.clear()
+    computer_cards = []
 
-    blackjack_new_card(player_cards_temp)
-    computer_cards.append(player_cards_temp)
-    blackjack_new_card(player_cards_temp)
-    computer_cards.append(player_cards_temp)
-    print("Computer card 2 = " + str(player_cards_temp))
+    blackjack_new_card(numbers)
+    computer_cards = [deck[numbers]]
+    blackjack_new_card(numbers)
+    computer_cards += [deck[numbers]]
+    print("Computer card 2 = " + str(computer_cards[1]))
     for z in range(int(player_count)):
-        blackjack_new_card(player_cards_temp)
-        player_cards.append(str(player_cards_temp) + ", ")
-        blackjack_new_card(player_cards_temp)
-        player_cards[z] += '%s,' % str(player_cards_temp)
+        blackjack_new_card(numbers)
+        player_cards.append(str(deck[numbers]) + ", ")
+        blackjack_new_card(numbers)
+        player_cards[z] += '%s, ' % str(deck[numbers])
         print(player_name[z] + "'s current cards = " + player_cards[z])
 
     for z in range(int(player_count)):
@@ -46,8 +45,8 @@ def blackjack_main(player_count):
         while draw_more:
             plus_one = input(player_name[z] + " do you want to draw another card? (if no press: n then enter )")
             if plus_one != "n":
-                blackjack_new_card(player_cards_temp)
-                player_cards[z] += '%s,' % player_cards_temp
+                blackjack_new_card(numbers)
+                player_cards[z] += '%s, ' % str(deck[numbers])
                 print(player_name[z] + "'s current cards = " + player_cards[z])
             else:
                 print("OK")
@@ -58,17 +57,14 @@ def blackjack_main(player_count):
         print(player_name[z] + "'s cards = " + player_cards[z])
 
 
-def blackjack_new_card(player_cards_temp):
-    number = random.randint(0, 52)
-    if number == 0:
-        blackjack_new_card(player_cards_temp)
-
-    player_card = [str(number)]
+def blackjack_new_card(numbers):
+    numbers = random.randint(0, 52)
+    print(str(numbers) + ", " + str(deck[numbers]))
+    player_card = [str(numbers)]
     if used_cards != player_card:
         used_cards.append(str(player_card))
-        player_cards_temp = deck[number]
     else:
-        blackjack_new_card(player_cards_temp)
+        blackjack_new_card(numbers)
 
 
 def blackjack_new_players(used, player, player_name):
@@ -88,4 +84,3 @@ def launcher():
 
 
 launcher()
-# blackjack_new_card(player_cards_temp)
